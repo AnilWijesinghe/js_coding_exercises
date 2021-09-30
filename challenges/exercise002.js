a841d98f8072276fba7aef587d94d58909fccaf4
Amali Fernando <amalifernando17@gmail.com>
2021-09-30 14:16:15 +0100

    exercise two completed

--- challenges/exercise002.js
+++ challenges/exercise002.js
@@ -1,26 +1,42 @@
 function getFillings(sandwich) {
   if (sandwich === undefined) throw new Error("ingredients is required");
-  // Your code here!
+  return sandwich.fillings;
 }
 
 function isFromManchester(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if(person.city=='Manchester'){
+    return true;
+  }else{
+    return false;
+  }
 }
 
 function getBusNumbers(people) {
   if (people === undefined) throw new Error("people is required");
-  // Your code here!
+  let value = Math.floor(people/40);
+  let remainder = people%40;
+  if(remainder==0){
+    return value;
+  }else{
+    return value+1;
+  }
 }
 
 function countSheep(arr) {
   if (arr === undefined) throw new Error("arr is required");
-  // Your code here!
+  return arr.filter(x => x === 'sheep').length;
 }
 
 function hasMPostCode(person) {
   if (person === undefined) throw new Error("person is required");
-  // Your code here!
+  if(!person.address.postCode.startsWith('M')){
+    return false;
+  }else if(person.address.postCode.startsWith('M') && person.address.city!='Manchester'){
+    return false;
+  }else{
+    return true;
+  }
 }
 
 module.exports = {
